7d123e6984510a85bd30a5e203a1bd41279a9e75
Konrad Dzwinel <kdzwinel@gmail.com>
2016-03-21 17:39:56 +0100

    Add build script - remove previous imperfect build method.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# clean up after last build
+rm -rf build/
+rm -rf bin/
+
+mkdir build/
+mkdir bin/
+
+# move files required by production app to the /build folder
+cp -r dt/ build/dt/
+cp -r lib/ build/lib/
+cp main.js build/
+cp package.json build/
+
+# install all dependencies
+cd build/
+npm i --production
+cd ..
+
+# build packages for all supported OS versions
+electron-packager ./build/ Betwixt --out ./bin/ --version=0.37.2 --platform=all --arch=all
